c2623b2a9a9daecd88d3e0846fd4cb2766b078c8
Kaiping Lu <kaipingl@umich.edu>
2024-04-03 18:41:17 -0400

    final commit with extra

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function MessageTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    let message = "";
+
+    if (hours >= 5 && hours < 12) { //Morning
+        message = "Good morning!";
+    } else if (hours >= 12 && hours < 18) { //Afternoon
+        message = "Good afternoon! 🌤";
+    } else if (hours >= 18 && hours < 21) { //Evening
+        message = "Good evening! 🌆";
+    } else { // Night
+        message = "Good night! 🌙";
+    }
+
+    document.getElementById('time-of-day-message').textContent = message;
+}
+
 function updateClock() {
     const now = new Date();
     const hours = now.getHours();
@@ -6,7 +24,8 @@ function updateClock() {
     const timeString = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 
     document.getElementById('live-clock').textContent = timeString;
-    setTimeout(updateClock, 1000); 
+    MessageTime()
+    setTimeout(updateClock, 1000);
 }
 
-document.addEventListener('DOMContentLoaded', updateClock); 
+document.addEventListener('DOMContentLoaded', updateClock);
